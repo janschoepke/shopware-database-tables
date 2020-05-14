8b92c06ebea4bf8ed970a82a95a4e61a615b2654
Jan Schöpke <janschoepke@ymail.com>
2020-05-14 20:52:21 +0200

    added sw6 support

--- src/js/main.js
+++ src/js/main.js
@@ -1,84 +1,139 @@
 var swVersions = {
-    "sw-5-2": [
-        { value: "sw-5-2-0", label: "5.2.0" },
-        { value: "sw-5-2-1", label: "5.2.1" },
-        { value: "sw-5-2-2", label: "5.2.2" },
-        { value: "sw-5-2-3", label: "5.2.3" },
-        { value: "sw-5-2-4", label: "5.2.4" },
-        { value: "sw-5-2-5", label: "5.2.5" },
-        { value: "sw-5-2-6", label: "5.2.6" },
-        { value: "sw-5-2-7", label: "5.2.7" },
-        { value: "sw-5-2-8", label: "5.2.8" },
-        { value: "sw-5-2-9", label: "5.2.9" },
-        { value: "sw-5-2-10", label: "5.2.10" },
-        { value: "sw-5-2-11", label: "5.2.11" },
-        { value: "sw-5-2-12", label: "5.2.12" },
-        { value: "sw-5-2-13", label: "5.2.13" },
-        { value: "sw-5-2-14", label: "5.2.14" },
-        { value: "sw-5-2-15", label: "5.2.15" },
-        { value: "sw-5-2-16", label: "5.2.16" },
-        { value: "sw-5-2-17", label: "5.2.17" },
-        { value: "sw-5-2-18", label: "5.2.18" },
-        { value: "sw-5-2-19", label: "5.2.19" },
-        { value: "sw-5-2-20", label: "5.2.20" },
-        { value: "sw-5-2-21", label: "5.2.21" },
-        { value: "sw-5-2-22", label: "5.2.22" },
-        { value: "sw-5-2-23", label: "5.2.23" },
-        { value: "sw-5-2-24", label: "5.2.24" },
-        { value: "sw-5-2-25", label: "5.2.25" },
-        { value: "sw-5-2-26", label: "5.2.26" },
-        { value: "sw-5-2-27", label: "5.2.27" }
-    ],
-    "sw-5-3": [
-        { value: "sw-5-3-0", label: "5.3.0" },
-        { value: "sw-5-3-1", label: "5.3.1" },
-        { value: "sw-5-3-2", label: "5.3.2" },
-        { value: "sw-5-3-3", label: "5.3.3" },
-        { value: "sw-5-3-4", label: "5.3.4" },
-        { value: "sw-5-3-5", label: "5.3.5" },
-        { value: "sw-5-3-6", label: "5.3.6" },
-        { value: "sw-5-3-7", label: "5.3.7" }
-    ],
-    "sw-5-4": [
-        { value: "sw-5-4-0", label: "5.4.0" },
-        { value: "sw-5-4-1", label: "5.4.1" },
-        { value: "sw-5-4-2", label: "5.4.2" },
-        { value: "sw-5-4-3", label: "5.4.3" },
-        { value: "sw-5-4-4", label: "5.4.4" },
-        { value: "sw-5-4-5", label: "5.4.5" },
-        { value: "sw-5-4-6", label: "5.4.6" }
-    ],
-    "sw-5-5": [
-        { value: "sw-5-5-0", label: "5.5.0" },
-        { value: "sw-5-5-1", label: "5.5.1" },
-        { value: "sw-5-5-2", label: "5.5.2" },
-        { value: "sw-5-5-3", label: "5.5.3" },
-        { value: "sw-5-5-4", label: "5.5.4" },
-        { value: "sw-5-5-5", label: "5.5.5" },
-        { value: "sw-5-5-6", label: "5.5.6" },
-        { value: "sw-5-5-7", label: "5.5.7" },
-        { value: "sw-5-5-8", label: "5.5.8" },
-        { value: "sw-5-5-9", label: "5.5.9" },
-        { value: "sw-5-5-10", label: "5.5.10" }
-    ],
-    "sw-5-6": [
-        { value: "sw-5-6-0", label: "5.6.0" },
-        { value: "sw-5-6-1", label: "5.6.1" },
-        { value: "sw-5-6-2", label: "5.6.2" },
-        { value: "sw-5-6-3", label: "5.6.3" },
-        { value: "sw-5-6-4", label: "5.6.4" }
-    ],
+    "sw-5": {
+        "sw-5-2": {
+            value: "sw-5|sw-5-2",
+            label: "5.2",
+            entries2: {
+                "sw-5-2-0": "5.2.0"
+            },
+            entries: {
+                "sw-5-2-0": {value: "sw-5-2-0", label: "5.2.0"},
+                "sw-5-2-1": {value: "sw-5-2-1", label: "5.2.1"},
+                "sw-5-2-2": {value: "sw-5-2-2", label: "5.2.2"},
+                "sw-5-2-3": {value: "sw-5-2-3", label: "5.2.3"},
+                "sw-5-2-4": {value: "sw-5-2-4", label: "5.2.4"},
+                "sw-5-2-5": {value: "sw-5-2-5", label: "5.2.5"},
+                "sw-5-2-6": {value: "sw-5-2-6", label: "5.2.6"},
+                "sw-5-2-7": {value: "sw-5-2-7", label: "5.2.7"},
+                "sw-5-2-8": {value: "sw-5-2-8", label: "5.2.8"},
+                "sw-5-2-9": {value: "sw-5-2-9", label: "5.2.9"},
+                "sw-5-2-10": {value: "sw-5-2-10", label: "5.2.10"},
+                "sw-5-2-11": {value: "sw-5-2-11", label: "5.2.11"},
+                "sw-5-2-12": {value: "sw-5-2-12", label: "5.2.12"},
+                "sw-5-2-13": {value: "sw-5-2-13", label: "5.2.13"},
+                "sw-5-2-14": {value: "sw-5-2-14", label: "5.2.14"},
+                "sw-5-2-15": {value: "sw-5-2-15", label: "5.2.15"},
+                "sw-5-2-16": {value: "sw-5-2-16", label: "5.2.16"},
+                "sw-5-2-17": {value: "sw-5-2-17", label: "5.2.17"},
+                "sw-5-2-18": {value: "sw-5-2-18", label: "5.2.18"},
+                "sw-5-2-19": {value: "sw-5-2-19", label: "5.2.19"},
+                "sw-5-2-20": {value: "sw-5-2-20", label: "5.2.20"},
+                "sw-5-2-21": {value: "sw-5-2-21", label: "5.2.21"},
+                "sw-5-2-22": {value: "sw-5-2-22", label: "5.2.22"},
+                "sw-5-2-23": {value: "sw-5-2-23", label: "5.2.23"},
+                "sw-5-2-24": {value: "sw-5-2-24", label: "5.2.24"},
+                "sw-5-2-25": {value: "sw-5-2-25", label: "5.2.25"},
+                "sw-5-2-26": {value: "sw-5-2-26", label: "5.2.26"},
+                "sw-5-2-27": {value: "sw-5-2-27", label: "5.2.27"}
+            },
+        },
+        "sw-5-3": {
+            value: "sw-5|sw-5-3",
+            label: "5.3",
+            entries: {
+                "sw-5-3-0": {value: "sw-5-3-0", label: "5.3.0"},
+                "sw-5-3-1": {value: "sw-5-3-1", label: "5.3.1"},
+                "sw-5-3-2": {value: "sw-5-3-2", label: "5.3.2"},
+                "sw-5-3-3": {value: "sw-5-3-3", label: "5.3.3"},
+                "sw-5-3-4": {value: "sw-5-3-4", label: "5.3.4"},
+                "sw-5-3-5": {value: "sw-5-3-5", label: "5.3.5"},
+                "sw-5-3-6": {value: "sw-5-3-6", label: "5.3.6"},
+                "sw-5-3-7": {value: "sw-5-3-7", label: "5.3.7"}
+            },
+        },
+        "sw-5-4": {
+            value: "sw-5|sw-5-4",
+            label: "5.4",
+            entries: {
+                "sw-5-4-0": {value: "sw-5-4-0", label: "5.4.0"},
+                "sw-5-4-1": {value: "sw-5-4-1", label: "5.4.1"},
+                "sw-5-4-2": {value: "sw-5-4-2", label: "5.4.2"},
+                "sw-5-4-3": {value: "sw-5-4-3", label: "5.4.3"},
+                "sw-5-4-4": {value: "sw-5-4-4", label: "5.4.4"},
+                "sw-5-4-5": {value: "sw-5-4-5", label: "5.4.5"},
+                "sw-5-4-6": {value: "sw-5-4-6", label: "5.4.6"}
+            },
+        },
+        "sw-5-5": {
+            value: "sw-5|sw-5-5",
+            label: "5.5",
+            entries: {
+                "sw-5-5-0": {value: "sw-5-5-0", label: "5.5.0"},
+                "sw-5-5-1": {value: "sw-5-5-1", label: "5.5.1"},
+                "sw-5-5-2": {value: "sw-5-5-2", label: "5.5.2"},
+                "sw-5-5-3": {value: "sw-5-5-3", label: "5.5.3"},
+                "sw-5-5-4": {value: "sw-5-5-4", label: "5.5.4"},
+                "sw-5-5-5": {value: "sw-5-5-5", label: "5.5.5"},
+                "sw-5-5-6": {value: "sw-5-5-6", label: "5.5.6"},
+                "sw-5-5-7": {value: "sw-5-5-7", label: "5.5.7"},
+                "sw-5-5-8": {value: "sw-5-5-8", label: "5.5.8"},
+                "sw-5-5-9": {value: "sw-5-5-9", label: "5.5.9"},
+                "sw-5-5-10": {value: "sw-5-5-10", label: "5.5.10"}
+            },
+        },
+        "sw-5-6": {
+            value: "sw-5|sw-5-6",
+            label: "5.6",
+            entries: {
+                "sw-5-6-0": {value: "sw-5-6-0", label: "5.6.0"},
+                "sw-5-6-1": {value: "sw-5-6-1", label: "5.6.1"},
+                "sw-5-6-2": {value: "sw-5-6-2", label: "5.6.2"},
+                "sw-5-6-3": {value: "sw-5-6-3", label: "5.6.3"},
+                "sw-5-6-4": {value: "sw-5-6-4", label: "5.6.4"}
+            }
+        }
+    },
+    "sw-6": {
+        "sw-6-0": {
+            value: "sw-6|sw-6-0",
+            label: "6.0",
+            entries: {
+                "sw-6-0-0-ea-1": {value: "sw-6-0-0-ea-1", label: "6.0.0 (EA1)"},
+                "sw-6-0-0-ea-1-1": {value: "sw-6-0-0-ea-1-1", label: "6.0.0 (EA1.1)"},
+                "sw-6-0-0-ea-1-2": {value: "sw-6-0-0-ea-1-2", label: "6.0.0 (EA2)"}
+            },
+        },
+        "sw-6-1": {
+            value: "sw-6|sw-6-1",
+            label: "6.1",
+            entries: {
+                "sw-6-1-0-rc-1": {value: "sw-6-1-0-rc-1", label: "6.1.0 (RC1)"},
+                "sw-6-1-0-rc-2": {value: "sw-6-1-0-rc-2", label: "6.1.0 (RC2)"},
+                "sw-6-1-0-rc-3": {value: "sw-6-1-0-rc-3", label: "6.1.0 (RC3)"},
+                "sw-6-1-0-rc-4": {value: "sw-6-1-0-rc-4", label: "6.1.0 (RC4)"},
+                "sw-6-1-0": {value: "sw-6-1-0", label: "6.1.0"},
+                "sw-6-1-1": {value: "sw-6-1-1", label: "6.1.1"},
+            }
+        }
+    }
 };
 
 $(function () {
     var $filter = $("#filter");
     var $specificVersion = $("#specific-version");
+    var $majorVersion = $('#major-version');
     var $ajaxContainer = $('#ajax-container');
+    var $softwareVersion = $('#software-version');
 
     function changeContent(filename) {
         $ajaxContainer.hide().load('sw-versions/' + filename + '.html', function () {
             $ajaxContainer.fadeIn(1000);
         });
+    }
+
+    function unsetFilter() {
+        $filter.val('');
+        $filter.trigger('keyup');
     }
 
     $filter.jcOnPageFilter({
@@ -93,26 +148,48 @@ $(function () {
     });
 
     $specificVersion.on('change', function () {
-        $filter.val('');
-        $filter.trigger('keyup');
+        unsetFilter();
         changeContent(this.value);
         $('.current-version').html($specificVersion.find("option:selected").html());
     });
 
-    $('#major-version').on('change', function () {
+    $majorVersion.on('change', function () {
+        unsetFilter();
         $specificVersion.find('option').remove();
-        var lastID = swVersions[this.value].length - 1;
+        var currentValue = this.value.split('|');
+        var currentSoftwareVersion = currentValue[0];
+        var currentMajorVersion = currentValue[1];
+        var lastID = Object.keys(swVersions[currentSoftwareVersion][currentMajorVersion].entries).length;
+        var counter = 0;
 
-        $.each(swVersions[this.value], function (i, item) {
+        $.each(swVersions[currentSoftwareVersion][currentMajorVersion].entries, function (i, item) {
             $specificVersion.append($('<option>', {
                 value: item.value,
                 text: item.label
             }));
-            if (i === lastID) {
+
+            if (++counter === lastID) {
                 $specificVersion.trigger('change');
             }
         });
     });
 
-    $specificVersion.trigger('change');
+    $softwareVersion.on('change', function() {
+        unsetFilter();
+        $majorVersion.find('option').remove();
+        var lastID = Object.keys(swVersions[this.value]).length;
+        var counter = 0;
+        $.each(swVersions[this.value], function (i, item) {
+            $majorVersion.append($('<option>', {
+                value: item.value,
+                text: item.label
+            }));
+
+            if (++counter === lastID) {
+                $majorVersion.trigger('change');
+            }
+        });
+    });
+
+    $softwareVersion.trigger('change');
 });
